0f60ff697f9d7afb1ec546d96a4a2f03a97c1886
vrianx <br.cerdav@duocuc.cl>
2022-08-22 18:57:47 -0400

    Tarea2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tarea2';
+
+
+  public Buenas():void{
+    alert('Buenas profe uwu');
+  }
+
+  public Choripan():void{
+    alert('Epico')
+  }
+
+  public divi2(n1:number, n2: number): number{
+    return n1 / n2;
+  }
+  public emoji():void{
+    alert('😳')
+  }
+
+
+
+
+
+
+
+
+
+
+
 }
